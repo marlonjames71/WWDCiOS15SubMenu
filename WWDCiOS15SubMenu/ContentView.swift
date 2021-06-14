0dbd556c237bc1e48e881714a0363bde4ec29835
Marlon Raskin <marlonr@hey.com>
2021-06-14 01:31:40 -0400

    Initial Commit

---
+++ WWDCiOS15SubMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WWDCiOS15SubMenu
+//
+//  Created by Marlon Raskin on 6/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WWDCiOS15SubMenu/WWDCiOS15SubMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDCiOS15SubMenuApp.swift
+//  WWDCiOS15SubMenu
+//
+//  Created by Marlon Raskin on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct WWDCiOS15SubMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
